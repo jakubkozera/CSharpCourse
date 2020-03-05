5af6fdf5552cac4b2c517e4571bfa9533d30a7b7
Jakub Kozera <kozerakuba@gmail.com>
2020-03-05 20:15:57 +0100

    Predefined types

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -10,6 +10,22 @@ namespace FirstProject
             string userName = Console.ReadLine();
             Console.WriteLine("Hello");
             Console.WriteLine(userName);
+
+            string someText = "Some text";
+
+            char jChar = 'j';
+            char jCharUnicode = '\u006A';
+
+            bool isUserReady = false;
+
+            DateTime now = DateTime.Now;
+            DateTime dateOfBirth = new DateTime(1990, 6, 6);
+
+            byte byteNumber = 200;
+            float floatNumber = 1.5F;
+            decimal decimalNumber = 1.5M;
+            double doubleNumber = 1.5;
+
         }
     }
 }
