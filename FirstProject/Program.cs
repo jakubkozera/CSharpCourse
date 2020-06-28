22b7a46dca419b673fbc5d7e08341c74cf62f088
Jakub Kozera <kozerakuba@gmail.com>
2020-06-28 09:19:13 +0200

    02-04-common-operators

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,34 +7,85 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            string message1;
-            message1 = "some value";
-            Console.WriteLine(message1);
+            int result;
+            int x = 10
+            int y = 5;
 
-            string message2 = null;
-            Console.WriteLine(message2);
-            string message3 = string.Empty;
-            string text = "He said \"Hi\"";
-            Console.WriteLine(text);
-            string windowLocation = "c:\\windows";
-            Console.WriteLine(windowLocation);
-            string fontsFolder = @"c:\windows\fonts";
-            Console.WriteLine(fontsFolder);
-            string concatenated = string.Concat(text, " to ", "me");
-            string concatenated2 = text + " to " + "me";
-            Console.WriteLine(concatenated);
-            Console.WriteLine(concatenated2);
+            result = (x + y);
+            Console.WriteLine("Addition Operator: x + y = " + result);
 
-            string interpolated = $"{text} to me";
-            Console.WriteLine(interpolated);
-            StringBuilder sb = new StringBuilder("This");
-            sb.Append("is");
-            sb.Append("a");
-            sb.Append("long");
-            sb.Append("text");
+            result = (x - y);
+            Console.WriteLine("Subtraction Operator: x - y = " + result);
 
-            string result = sb.ToString();
-            Console.WriteLine(result);
+            result = (x * y);
+            Console.WriteLine("Multiplication Operator: x * y = " + result);
+
+            result = (x / y);
+            Console.WriteLine("Division Operator: x / y = " + result);
+
+            result = (x % y);
+            Console.WriteLine("Modulo Operator: x % y = " + result);
+
+            int value = (2 + 2) * 5; //20
+
+            Console.WriteLine("*******************");
+
+            int value1 = 10;
+            int value2 = 10;
+
+            Console.WriteLine("Pre-increment result:");
+            Console.WriteLine(++value1);
+
+
+            Console.WriteLine("Post-increment result:");
+            Console.WriteLine(value2++);
+            Console.WriteLine(value2);
+
+            Console.WriteLine("***************");
+            bool boolResult;
+            x = 2;
+            y = 10;
+
+            boolResult = (x == y);
+            Console.WriteLine("Equal to Operator: (x == y) = " + boolResult);
+
+            boolResult = (x > y);
+            Console.WriteLine("Greater than Operator: (x > y) = " + boolResult);
+
+            boolResult = (x < y);
+            Console.WriteLine("Less than Operator: (x < y) = " + boolResult);
+
+            boolResult = (x >= y);
+            Console.WriteLine("Greater than or Equal to: (x >= y) =  " + boolResult);
+
+            boolResult = (x <= y);
+            Console.WriteLine("Lesser than or Equal to: (x <= y) = " + boolResult);
+
+            boolResult = (x != y);
+            Console.WriteLine("Not Equal to Operator: (x != y) = " + boolResult);
+
+            Console.WriteLine("***************");
+
+            bool a = true;
+            bool b = false;
+
+            boolResult = a && b;
+            Console.WriteLine("AND Operator: a && b = " + boolResult);
+
+            boolResult = a || b;
+            Console.WriteLine("OR Operator: a || b = " + boolResult);
+
+            boolResult = !a;
+            Console.WriteLine("NOT Operator: !a = " + boolResult);
+
+            Console.WriteLine("***************");
+            x = 5;
+            y = 10;
+
+            int greaterNumber = x > y ? x : y;
+            Console.WriteLine("Ternary conditional operator: x > y ? x : y = " + greaterNumber);
+
+
         }
     }
 }
