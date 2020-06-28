38f1fc89d975aa84b5bedb6ae81c7099f722c1dd
Jakub Kozera <kozerakuba@gmail.com>
2020-06-28 10:00:29 +0200

    02-05-comments-and-regions

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,40 +1,31 @@
 ﻿using System;
 using System.Text;
+using System.Threading.Channels;
 
 namespace FirstProject
 {
+    #region ProgramClassRegion
     class Program
     {
+        /// <summary>
+        /// main method description
+        /// </summary>
+        /// <param name="args">args description</param>
         static void Main(string[] args)
         {
-            string message1;
-            message1 = "some value";
-            Console.WriteLine(message1);
+            Console.WriteLine("");
 
-            string message2 = null;
-            Console.WriteLine(message2);
-            string message3 = string.Empty;
-            string text = "He said \"Hi\"";
-            Console.WriteLine(text);
-            string windowLocation = "c:\\windows";
-            Console.WriteLine(windowLocation);
-            string fontsFolder = @"c:\windows\fonts";
-            Console.WriteLine(fontsFolder);
-            string concatenated = string.Concat(text, " to ", "me");
-            string concatenated2 = text + " to " + "me";
-            Console.WriteLine(concatenated);
-            Console.WriteLine(concatenated2);
+            int value = (2 + 2) * 5; //20
+            // this is a single line comment
 
-            string interpolated = $"{text} to me";
-            Console.WriteLine(interpolated);
-            StringBuilder sb = new StringBuilder("This");
-            sb.Append("is");
-            sb.Append("a");
-            sb.Append("long");
-            sb.Append("text");
-
-            string result = sb.ToString();
-            Console.WriteLine(result);
+            /* This
+            is 
+            a 
+            multi-line comment */
         }
     }
+    #endregion
+
 }
+
+
