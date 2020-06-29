e80a1b8b859fb3433513b5bb7df6255675cc6aa3
Jakub Kozera <kozerakuba@gmail.com>
2020-06-29 14:23:30 +0200

    02-06-if-else

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -4,28 +4,31 @@ using System.Threading.Channels;
 
 namespace FirstProject
 {
-    #region ProgramClassRegion
     class Program
     {
-        /// <summary>
-        /// main method description
-        /// </summary>
-        /// <param name="args">args description</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("Year of birth?");
+            string userInput = Console.ReadLine();
 
-            int value = (2 + 2) * 5; //20
-            // this is a single line comment
+            int yearOfBirth = int.Parse(userInput);
+            bool isUserOver18 = DateTime.Now.Date.Year - yearOfBirth > 18;
+            if (isUserOver18)
+            {
+                Console.WriteLine("hello");
+            }
+            else if(DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("Oh well its Sunday");
+            }
+            else
+            {
+                Console.WriteLine("Access denied");
+            }
 
-            /* This
-            is 
-            a 
-            multi-line comment */
+            Console.WriteLine("bye");
         }
     }
-    #endregion
-
 }
 
 
