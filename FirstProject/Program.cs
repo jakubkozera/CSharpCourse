f22bf8ea9804d708dc2af7db880f8bdbeed61f35
Jakub Kozera <kozerakuba@gmail.com>
2020-07-04 11:35:33 +0200

    Arrays

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,28 +7,15 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            byte byteValue = 100;
-            int intValue = byteValue;
-            byte byteValue2 = (byte)intValue;
-            double doubleValue = 3.5;
-            int intValue2 = (int)doubleValue;
+            string[] cars = { "Volvo", "BMW", "Mazda" };
 
+            Console.WriteLine(cars[0]);
+            cars[2] = "Tesla";
 
-            string stringValue2 = intValue2.ToString();
+            int carsCount = cars.Length; // 3
 
-            string userInput = Console.ReadLine();
-            int yearOfBirth;
-            if (int.TryParse(userInput, out yearOfBirth))
-            {
-                int age = DateTime.Now.Year - yearOfBirth;
-
-                Console.WriteLine("You are " + age);
-            }
-            else
-            {
-                Console.WriteLine("Incorrect value");
-            }
-
+            string lastElement = cars[cars.Length - 1];
+            Console.WriteLine(lastElement);
 
         }
     }
