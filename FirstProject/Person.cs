f2dcfcc8edcfea688bec4a1c47aff95d8260e2f4
Jakub Kozera <kozerakuba@gmail.com>
2020-07-13 20:23:46 +0200

    methods

--- FirstProject/Person.cs
+++ FirstProject/Person.cs
@@ -9,6 +9,25 @@ namespace FirstProject
         public string FirstName;
         public string LastName;
 
-        public DateTime DateOfBirth;
+        private DateTime dateOfBirth;
+
+        public void SetDateOfBirth(DateTime date)
+        {
+            if (date > DateTime.Now)
+            {
+                Console.WriteLine("Invalid date of birth");
+            }
+            else
+            {
+                dateOfBirth = date;
+            }
+        }
+
+        public DateTime GetDateOfBirth() => dateOfBirth;
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {FirstName}, {GetDateOfBirth()}");
+        }
     }
 }
--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -9,8 +9,9 @@ namespace FirstProject
             Person bill = new Person();
 
             bill.FirstName = "Bill";
+            bill.SetDateOfBirth(new DateTime(1990, 1, 1));
 
-            Console.WriteLine($"{bill.FirstName} {bill.DateOfBirth}");
+            bill.SayHi();
 
         }
     }
