c574e44d5c2e36df4f259ea2a9ae1fc67d683d84
Jakub Kozera <kozerakuba@gmail.com>
2020-09-05 11:36:59 +0200

    04-05-Dictionary

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -8,27 +8,27 @@ namespace FirstProject
 {
     class Program
     {
-        static Dictionary<string, Currency> GetCurrencies()
+        static List<Currency> GetCurrencies()
         {
-            return new Dictionary<string, Currency>
+            return new List<Currency>
             {
-                { "usd", new Currency("usd", "United States, Dollar", 1)},
-                { "eur", new Currency("eur", "Euro", 0.83975)},
-                { "gbp", new Currency("gbp", "British Pound", 0.74771)},
-                { "cad", new Currency("cad", "Canadian Dollar", 1.30724)},
-                { "inr", new Currency("inr", "Indian Rupee", 73.04)},
-                { "mxn", new Currency("mxn", "Mexican Peso", 21.7571)}
+                new Currency("usd", "United States, Dollar", 1),
+                new Currency("eur", "Euro", 0.83975),
+                new Currency("gbp", "British Pound", 0.74771),
+                new Currency("cad", "Canadian Dollar", 1.30724),
+                new Currency("inr", "Indian Rupee", 73.04),
+                new Currency("mxn", "Mexican Peso", 21.7571)
             };
         }
 
         static void Main(string[] args)
         {
-            Dictionary<string, Currency> currencies = GetCurrencies();
+            List<Currency> currencies = GetCurrencies();
             Console.WriteLine("Check the rate for:");
             string userInput = Console.ReadLine();
 
-            Currency selectedCurrency = null;
-            if (currencies.TryGetValue(userInput, out selectedCurrency))
+            Currency selectedCurrency = currencies.FirstOrDefault(c => c.Name == userInput);
+            if (selectedCurrency == null)
             {
                 Console.WriteLine($"Rate for USD to {selectedCurrency.FullName} is {selectedCurrency.Rate}");
             }
@@ -36,9 +36,6 @@ namespace FirstProject
             {
                 Console.WriteLine("Currency not found");
             }
-
-            currencies.Remove("usd");
-            currencies.TryAdd("usd", new Currency("usd", "Dollar", 1));
         }
 
     }
