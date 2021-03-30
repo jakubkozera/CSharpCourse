caa0b43606de94f3ad8e636ff447cd57050ff860
Jakub Kozera <kozerakuba@gmail.com>
2021-03-30 19:26:48 +0200

    09-01 yield

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -8,47 +8,51 @@ namespace FirstProject
 {
     class Program
     {
-        public delegate void Display(string value);
-
-        public delegate bool GenericPredicate<T>(T value);
-
-        static void Main(string[] args)
+        public static IEnumerable<int> GetData()
         {
+            Console.WriteLine("Get data start");
 
-            Display display = (string value) => Console.Write(value + ", ");
-
-            display("Test");
-
-            var numbers = new int[] {10, 30, 50};
-            //DisplayNumbers(number, display);
-            var count = Count(numbers, (int value) => value > 35);
-            Console.WriteLine(count);
-
-            var strings = new string[] {"Generic", "Delegate", "test"};
-            var countString = Count(strings, value => value.Length > 4);
-            Console.WriteLine($"countString {countString}");
-        }
-
-        static void DisplayNumbers(IEnumerable<int> numbers, Display display)
-        {
-            foreach (int number in numbers)
+            var result = new List<int>();
+            for (int i = 0; i <= 9; i++)
             {
-                display(number.ToString());
+                Console.WriteLine($"Get data element: {i}");
+                result.Add(i);
             }
-        }
 
-        static int Count<T>(IEnumerable<T> elements, Predicate<T> predicate)
+            Console.WriteLine("Get data end");
+
+            return result;
+        }
+        public static IEnumerable<int> GetYieldedData()
         {
-            int count = 0;
-            foreach (T element in elements)
+            Console.WriteLine("GetYieldedData start");
+
+            for (int i = 1; i <= 9; i++)
             {
-                if (predicate(element))
+                Console.WriteLine($"GetYieldedData element: {i}");
+                yield return i;
+                if (i % 3 == 0)
                 {
-                    count++;
+                    yield break;
                 }
             }
 
-            return count;
+            Console.WriteLine("GetYieldedData stop");
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            var data = GetYieldedData();
+            foreach (int element in data)
+            {
+                Console.WriteLine($"main element: {element}");
+                if (element > 5)
+                {
+                    break;
+                }
+            }
         }
 
     }
