1dd492158a55dd8daa9801dd7bc97c57c1293716
Jakub Kozera <kozerakuba@gmail.com>
2021-04-28 08:52:32 +0200

    11-01 cleanup to base

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,11 +1,5 @@
 ﻿// ReSharper disable All
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 
@@ -13,49 +7,12 @@ namespace FirstProject
 {
     class Program
     {
-        static async Task FooAsync()
-        {
-            Console.WriteLine("Foo start");
-
-            await Task.Delay(2000);
-
-            throw new Exception();
-
-            Console.WriteLine("Foo end");
-
-        }
-
-        private static string userAnswer;
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Main started");
-            var tokenSource = new CancellationTokenSource(1000);
-            CancellationToken token = tokenSource.Token;
 
-            var userAnswer = GetAnswer();
-            var delay = Task.Delay(5000, token);
-
-            var returnedTask = await Task.WhenAny(userAnswer, delay);
-
-            if (delay.IsCompleted)
-            {
-                Console.WriteLine($"Your anwser is incorret");
-            }
-
-
-
-
-            Console.ReadKey();
 
         }
 
-        static Task<string> GetAnswer()
-        {
-            Console.WriteLine("Your answer:");
-            userAnswer = Console.ReadLine();
-
-            return Task.FromResult(userAnswer);
-        }
     }
 }
 
