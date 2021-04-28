2526c37091170f4b51ad39902827e56c35f9e409
Jakub Kozera <kozerakuba@gmail.com>
2021-04-28 11:09:32 +0200

    11 01 http client

---
+++ FirstProject/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Body { get; set; }
+        public string Title { get; set; }
+    }
+}
--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,6 +1,11 @@
 ﻿// ReSharper disable All
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
 
 
 namespace FirstProject
@@ -9,6 +14,51 @@ namespace FirstProject
     {
         static async Task Main(string[] args)
         {
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
+                var json = await result.Content.ReadAsStringAsync();
+
+                var posts = JsonConvert.DeserializeObject<List<Post>>(json);
+
+                var selectedPost = posts.First(p => p.Id == 30);
+                Console.WriteLine(selectedPost.Title);
+                Console.WriteLine(selectedPost.Body);
+
+                selectedPost.Title = "test title";
+                selectedPost.Body = "test body";
+
+                Console.WriteLine(selectedPost.Title);
+                Console.WriteLine(selectedPost.Body);
+
+                var postJsonContent = new StringContent(JsonConvert.SerializeObject(selectedPost));
+
+                var postResult = await httpClient
+                    .PostAsync("https://jsonplaceholder.typicode.com/posts", postJsonContent);
+
+
+                using (var postRequestMessage =
+                    new HttpRequestMessage(HttpMethod.Post, "https://jsonplaceholder.typicode.com/posts"))
+                {
+
+                    //postRequestMessage.Headers.Add("someheader", "somevalue");
+                    postRequestMessage.Content = postJsonContent;
+
+                    var post2Result = await httpClient.SendAsync(postRequestMessage);
+
+                }
+
+                var queryParams = HttpUtility.ParseQueryString(string.Empty);
+                queryParams["postId"] = "1";
+                queryParams["someParam"] = "someValue";
+
+                var formattedParams = queryParams.ToString();
+
+                Console.WriteLine("Formatted params:");
+                Console.WriteLine(formattedParams);
+
+
+            }
 
 
         }
