ca0c1f0baf7a5c4828fbaee4fb5d43ec45640219
Jakub Kozera <KOZERJ01@heiway.net>
2022-02-10 16:27:26 +0100

    02-18-regex

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,54 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Runtime.CompilerServices;
-// ReSharper disable InlineOutVariableDeclaration
-#pragma warning disable 219
+using System.Text.RegularExpressions;
 
 namespace FirstProject
 {
-    class Program
+    public class Program
     {
-        static Dictionary<string, Currency> GetCurrencies()
+        private static void Main(string[] args)
         {
-            return new Dictionary<string, Currency>
-            {
-                { "usd", new Currency("usd", "United States, Dollar", 1)},
-                { "eur", new Currency("eur", "Euro", 0.83975)},
-                { "gbp", new Currency("gbp", "British Pound", 0.74771)},
-                { "cad", new Currency("cad", "Canadian Dollar", 1.30724)},
-                { "inr", new Currency("inr", "Indian Rupee", 73.04)},
-                { "mxn", new Currency("mxn", "Mexican Peso", 21.7571)}
-            };
+            Regex regex = new Regex(@"^([a-z0-9]+)\.?([a-z0-9]+)@([a-z]+)\.[a-z]{2,3}$");
+            string email = "testm.test2@gmail.com";
+
+            bool isMatch = regex.IsMatch(email);
+            Console.WriteLine(isMatch);
         }
-
-        static void Main(string[] args)
-        {
-            var currencies = GetCurrencies();
-            Console.WriteLine("Check the rate for:");
-            var userInput = Console.ReadLine();
-
-            var index = 1;
-
-            var someText = "tests";
-
-            Currency selectedCurrency = null;
-            if (currencies.TryGetValue(userInput, out selectedCurrency))
-            {
-                Console.WriteLine($"Rate for USD to {selectedCurrency.FullName} is {selectedCurrency.Rate}");
-            }
-            else
-            {
-                Console.WriteLine("Currency not found");
-            }
-
-            currencies.Remove("usd");
-            currencies.TryAdd("usd", new Currency("usd", "United States, Dollar", 1));
-        }
-
     }
 }
-
-
-#pragma warning restore 219
