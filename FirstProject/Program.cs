3f66492a729cfb9ecf5beef37711305e118e3f4f
Adrian <adrianmetowski@sourceful.nl>
2023-05-11 14:16:19 +0200

    ready check if reservation is possible feature

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -37,10 +37,21 @@ namespace FirstProject
 
         static bool IsNewReservationPossible(DateTime startDate, DateTime endDate, List<Reservation> bookedReservations)
         {
-            //TODO: Implement the logic
+            foreach(var bookedReservation in bookedReservations)
+            {
+                if(startDate.Date >= bookedReservation.From.Date && startDate.Date <= bookedReservation.To.Date || 
+                    endDate.Date >= bookedReservation.From.Date && endDate.Date <= bookedReservation.To.Date)
+                {
+                    return false;
+                }
 
+                if(startDate.Date <= bookedReservation.From.Date && endDate.Date >= bookedReservation.To.Date)
+                {
+                    return false;
+                }
+            }
 
-            return false;
+            return true;
         }
 
         static void DisplayReservations(List<Reservation> bookedReservations)
